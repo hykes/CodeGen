c037f94cf39ceb14d4dfc4ef848e0c4b5afb4c19
hehaiyangwork <hehaiyangwork@qq.com>
2017-04-01 01:14:49 +0800

    add javaHelp

---
+++ resources/javahelp/hello.hs
@@ -0,0 +1,24 @@
+<?xml version='1.0' encoding='utf-8' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="1.0">
+  <title>Hello, JavaHelp</title>
+  <maps>
+    <mapref location="Map.jhm"/>
+    <homeID>overview</homeID>
+  </maps>
+  <view>
+    <name>TOC</name>
+    <label>TOC</label>
+    <type>javax.help.TOCView</type>
+    <data>toc.xml</data>
+  </view>
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>index.xml</data>
+  </view>
+</helpset>
