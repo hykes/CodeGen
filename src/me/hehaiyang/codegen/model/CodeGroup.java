0c5d8621e039e5d1c243f2604d5c01168005b4f6
hehaiyangwork <hehaiyangwork@qq.com>
2017-06-06 23:38:13 +0800

    add codeGroup

---
+++ src/me/hehaiyang/codegen/model/CodeGroup.java
@@ -0,0 +1,40 @@
+package me.hehaiyang.codegen.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+import java.util.List;
+
+/**
+ * Desc:
+ * Mail: hehaiyang@terminus.io
+ * Date: 2017/6/6
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CodeGroup implements Serializable {
+
+    private static final long serialVersionUID = -8843957173498883576L;
+
+    /**
+     * 分组ID
+     */
+    private String id;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 优先级
+     */
+    private Integer level;
+
+    private List<CodeTemplate> templates;
+
+}
