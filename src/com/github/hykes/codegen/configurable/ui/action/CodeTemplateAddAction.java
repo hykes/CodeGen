8ad6e790e26588736eb6748c5fb912a69e465c3f
IceMimosa <chk@terminus.io>
2018-01-13 23:42:00 +0800

    Remove useless

--- src/com/github/hykes/codegen/configurable/ui/action/CodeTemplateAddAction.java
+++
@@ -1,7 +0,0 @@
-package com.github.hykes.codegen.configurable.ui.action;
-
-import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-
-
